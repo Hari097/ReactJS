d7c7c9ddecb1d52f5627e7ebd8631c1df1520aaa
Hari097 <harikalam143@gmail.com>
2024-05-28 12:47:03 +0530

    letters count input

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,10 +1,32 @@
-import logo from './logo.svg';
+import LettersCalculator from './Componests/LettersCalculator';
 import './App.css';
+import { Component } from 'react';
+class App extends Component {
 
-function App() {
-  return (
-  <h1>Hello World</h1>
-  );
+
+
+
+  render() {
+
+
+  return(
+    <div className='letter-card-container'>
+
+<div className='letter-card'>
+  <div className='heading-card'>
+    <h1 className='heading'>Calculate the lettes you enters</h1>
+  </div>
+
+  <div className='image-card'>
+  <img src='https://assets.ccbp.in/frontend/react-js/stop-watch-with-calculator-img.png' alt='image' className='image'/>
+  </div>
+
+</div>
+
+      <LettersCalculator/>
+    </div>
+  )
+  }
 }
 
 export default App;
