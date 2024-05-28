ea0f767b58da63619129be701240d9bb5a03f4c4
Hari097 <harikalam143@gmail.com>
2024-05-28 21:48:22 +0530

    browserHistory search

---
+++ reactapp/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css';
+import { Component } from 'react';
+import BrowserHistory from './Componests/BrowserHistory';
+
+
+class App extends Component {
+
+  render() {
+
+  return(
+   <div>
+    <BrowserHistory/>
+   </div>
+  )
+  }
+}
+
+export default App;
+
